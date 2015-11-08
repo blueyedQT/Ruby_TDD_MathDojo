3ca0523251451cbd770cb05a1acec11aea631cc1
Katrina <blueyed_qt@hotmail.com>
2015-11-08 11:27:58 -0800

    Added attribue and methods: add and subtract

--- mathdojo.rb
+++ mathdojo.rb
@@ -1,2 +1,29 @@
 class MathDojo
+	attr_accessor :answer
+
+	def initialize 
+		@answer = 0
+	end
+
+	def add value
+		if value.kind_of? Array
+			value.each do |i|
+				@answer += i
+			end
+		else
+			@answer += value
+		end
+		self
+	end
+
+	def subtract value
+		if value.kind_of? Array
+			value.each do |i|
+				@answer -= i
+			end
+		else
+			@answer -= value
+		end
+		self
+	end
 end
